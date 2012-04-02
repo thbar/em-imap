dfecb3b195c7c187f15f7a9477710bf1ccce85bc
Rick Winfrey <rick.winfrey@gmail.com>
2012-04-02 16:33:47 -0500

    yo dawg go fetch yo' uid

--- lib/net/imap.rb
+++ lib/net/imap.rb
@@ -1446,8 +1446,8 @@ module Net
             ":" + format_internal(data.last)
         when Array
           command_request = data.collect {|i| format_internal(i)}.join(",")
-          puts command_request << " UID"
-          return append.nil? ? command_request : command_request << " UID"
+          puts "UID FETCH " << command_request << " UID"
+          return append.nil? ? command_request : "UID FETCH " << command_request << " UID"
         when ThreadMember
           return data.seqno.to_s +
             ":" + data.children.collect {|i| format_internal(i).join(",")}
